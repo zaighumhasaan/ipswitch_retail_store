b8e5a8c753d5910c171f3ed112f859400333fdcd
Zaighum Hassan Bhatti <61985553+zaighumhasaan@users.noreply.github.com>
2026-02-25 20:17:22 +0000

    Change image upload path to 'product_images/'

--- store/models.py
+++ store/models.py
@@ -26,7 +26,7 @@ class Product(models.Model):
     slug = models.SlugField(max_length=200, unique=True)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    image = models.ImageField(upload_to='products/', blank=True)
+    image = models.ImageField(upload_to='product_images/', blank=True)
     stock = models.PositiveIntegerField(default=0)
     available = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
